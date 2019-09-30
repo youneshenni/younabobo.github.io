ec589a2bb364f3d5772dfa16374fc1dc2b910674
Younes Henni <youn.henni@gmail.com>
2019-09-30 12:29:40 +0100

    T

---
+++ precache-manifest.a15c07785a7104f9233d7fefea498ea0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92134938477bc492a640bef3e232d76e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14032b6746f2de49240a",
+    "url": "/static/js/2.a2df9e10.chunk.js"
+  },
+  {
+    "revision": "080aa322a4d0a65e81b8",
+    "url": "/static/js/main.582b6662.chunk.js"
+  },
+  {
+    "revision": "b9b38f2841eadb100253",
+    "url": "/static/js/runtime-main.675d1fba.js"
+  }
+]);
